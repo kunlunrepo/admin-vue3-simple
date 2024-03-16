bebd13aaa407c70d0d798d94c6a19cb61780f98c
kunlunrepo <482976531@qq.com>
2024-03-16 22:51:25 +0800

    新增图标大小测试验证

---
+++ src/components/Icon/Iconify.vue
@@ -0,0 +1,15 @@
+<template>
+  <component :is="Icon" v-bind="props" />
+</template>
+
+<script lang="ts" setup>
+import {Icon, type IconProps} from "@iconify/vue";
+
+const props = defineProps<IconProps>();
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/icon-test.vue
@@ -0,0 +1,11 @@
+<template>
+  <Iconify icon="material-symbols:11mp-outline-rounded"></Iconify>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
