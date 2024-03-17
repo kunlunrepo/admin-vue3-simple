b3632fbbae8ebe8d40e99d36bd43bb1575e129d6
kunlunrepo <482976531@qq.com>
2024-03-17 18:27:50 +0800

    新增通知消息功能

---
+++ src/components/Notice/Notification.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-badge :value="value" :style="{
+        '--toimc-notice-bg-color': color || 'var(--el-color-danger)',
+        '--toimc-notice-font-size': size + 'px' || 'var(--el-badge-size)'
+    }">
+        <slot>
+            <Icon icon="ep:bell" class="text-3xl"
+                  :style="{color: iconColor ?? '#333',
+                  fontSize: iconSize ? `${iconSize}px` : '18px'}"></Icon>
+        </slot>
+    </el-badge>
+</template>
+
+<script setup lang="ts">
+import Icon from "../Icon/Iconify.vue";
+import {BadgeProps} from "element-plus";
+import type {IconifyIcon} from "@iconify/vue";
+
+interface NotificationProps extends Partial<BadgeProps> {
+    icon: string | IconifyIcon
+    iconSize?: number
+    iconColor?: string
+    size: number
+    color: string
+}
+
+withDefaults(defineProps<NotificationProps>(), {
+    icon: 'ep:bell',
+    size: 12,
+    color: ''
+})
+
+
+</script>
+
+<style scoped lang="scss">
+$color: var(--toimc-notice-bg-color);
+$size: var(--toimc-notice-font-size);
+:deep(.el-badge__content) {
+    background-color: $color;
+    font-size: $size;
+}
+</style>
---
+++ src/pages/components/notice/notice-message.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      通知组件示例
+      <Notification value="123456"></Notification>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Notification from "@/components/Notice/Notification.vue";
+</script>
+
+<style scoped lang="scss">
+</style>
