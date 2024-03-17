b61a46a73382298f13567ddab78425365972cc7f
kunlunrepo <482976531@qq.com>
2024-03-17 18:43:56 +0800

    新增通知消息功能，使用缩放控制大小

--- src/components/Notice/Notification.vue
+++ src/components/Notice/Notification.vue
@@ -1,7 +1,9 @@
 <template>
     <el-badge :value="value" :style="{
-        '--toimc-notice-bg-color': color || 'var(--el-color-danger)',
-        '--toimc-notice-font-size': size + 'px' || 'var(--el-badge-size)'
+        '--bg-color': color || 'var(--el-color-danger)',
+        '--font-size': size + 'px' || 'var(--el-badge-size)',
+        '--translate-x': (transformData.translateX || 100) + '%',
+        '--scale': transformData.scale
     }">
         <slot>
             <Icon icon="ep:bell" class="text-3xl"
@@ -21,23 +23,49 @@ interface NotificationProps extends Partial<BadgeProps> {
     iconSize?: number
     iconColor?: string
     size: number
+    scale: number
     color: string
 }
 
-withDefaults(defineProps<NotificationProps>(), {
+const props = withDefaults(defineProps<NotificationProps>(), {
     icon: 'ep:bell',
     size: 12,
+    scale: 1,
     color: ''
 })
 
+// tranformX -> scale
+function calculateTransform(scale: number) {
+    // 定义scale和translateX的范围
+    const minScale = 0.4
+    const maxScale = 1
+    const minTranslateX = 75 // 单位：%
+    const maxTranslateX = 100 // 单位：%
+
+    // 计算translateX的值
+    const translateX =
+        minTranslateX + (maxTranslateX - minTranslateX) * ((scale - minScale) / (maxScale - minScale))
+
+    // 返回完整的transform属性值
+    return {
+        translateX: translateX,
+        scale
+    }
+}
+
+const transformData = computed(() => calculateTransform(props.scale))
 
 </script>
 
 <style scoped lang="scss">
-$color: var(--toimc-notice-bg-color);
-$size: var(--toimc-notice-font-size);
+$color: var(--bg-color);
+$size: var(--font-size);
+$translate-x: var(--translate-x);
+$scale: var(--scale);
+
 :deep(.el-badge__content) {
     background-color: $color;
     font-size: $size;
+    transform: translateY(-50%) translateX($translate-x) scale($scale);
 }
 </style>
--- src/pages/components/notice/notice-message.vue
+++ src/pages/components/notice/notice-message.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
       通知组件示例
-      <Notification value="123456"></Notification>
+      <Notification value="123456" :scale="scale">
+      </Notification>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      {{scale}}
+      <el-button @click="scale=0.5">点击</el-button>
+      <el-button @click="scale=1">点击</el-button>
+      <el-button @click="scale=1.5">点击</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import Notification from "@/components/Notice/Notification.vue";
+
+const scale = ref(0.5);
 </script>
 
 <style scoped lang="scss">
